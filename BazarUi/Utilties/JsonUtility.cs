acd9d0b2691f107523b861fad9b775f1a5d8bd87
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-11-05 10:21:15 +0200

    Add Utility Class

---
+++ BazarUi/Utilties/JsonUtility.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using BazarUi.Models;
+
+namespace BazarUi.Utilties
+{
+    public class JsonUtility
+    {
+        public static List<T> DeserializeSearchResults<T>(string json)
+        {
+            List<T> results = new List<T>();
+
+            try
+            {
+                results = JsonSerializer.Deserialize<List<T>>(json);
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Error deserializing JSON: {ex.Message}");
+            }
+
+            return results;
+        }
+    }
+
+}
