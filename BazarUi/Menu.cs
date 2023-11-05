72539c26e9866ce3c1539b16c6696ec5ff253296
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-11-05 10:22:40 +0200

    Feature: Build The FrontEnd Service With Mock Apis

---
+++ BazarUi/Menu.cs
@@ -0,0 +1,85 @@
+﻿namespace BazarUi
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Menu
+    {
+        private Dictionary<int, string> menuItems;
+        private readonly Operations operations;
+        public Menu(Operations operations)
+        {
+            menuItems = new Dictionary<int, string>
+        {
+            { 1, "Search by topic" },
+            { 2, "Info for a certian book" },
+            { 3, "Purchase a book" },
+            { 4, "Exit" }
+        };
+            this.operations = operations;
+        }
+
+        public void Run()
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                DisplayMenu();
+                int choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter a topic: ");
+                        string topic = Console.ReadLine();
+                        operations.Search(topic);
+                        break;
+                    case 2:
+                        Console.Write("Enter an item number: ");
+                        int itemNumber = GetIntInput();
+                        operations.Info(itemNumber);
+                        break;
+                    case 3:
+                        Console.Write("Enter an item number for purchase: ");
+                        int purchaseItemNumber = GetIntInput();
+                        operations.Purchase(purchaseItemNumber);
+                        break;
+                    case 4:
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please select a valid option.");
+                        break;
+                }
+            }
+        }
+
+        private void DisplayMenu()
+        {
+            Console.WriteLine("Menu:");
+            foreach (var menuItem in menuItems)
+            {
+                Console.WriteLine($"{menuItem.Key}. {menuItem.Value}");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Select an option: ");
+            return GetIntInput();
+        }
+
+        private int GetIntInput()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Invalid input. Please enter a valid number: ");
+            }
+            return number;
+        }
+
+    }
+
+}
---
+++ BazarUi/Program.cs
@@ -0,0 +1,16 @@
+﻿using BazarUi.Utilties;
+
+namespace BazarUi
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+            Menu mainMenu= new Menu(operations);
+            mainMenu.Run();
+           
+        }
+    }
+}
