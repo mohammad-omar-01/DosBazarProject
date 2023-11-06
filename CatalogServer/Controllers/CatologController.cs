aea3eb230c7056850441d7fb96f9321a5c894ec1
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-11-06 12:19:16 +0200

    Feature:Search Api Endpoint, info Api Endpoint

---
+++ CatalogServer/Controllers/CatologController.cs
@@ -0,0 +1,36 @@
+﻿using CatalogServer.Bazar.Db;
+using CatalogServer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogServer.Controllers
+{
+    [ApiController]
+    public class CatologController : Controller
+    {
+        BazarDbRepository bazarDbRepository = new BazarDbRepository();
+
+        [HttpGet("/Book/{BookId}")]
+        public ActionResult<Book> GetBookById(int BookId)
+        {
+            var book = bazarDbRepository.RetriveBookById(BookId);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpGet("Search/{Topic}")]
+        public ActionResult<List<BookSerachTopicDTO>> GetBookByTopic(string Topic)
+        {
+            var books = bazarDbRepository.RetriveBookByTopic(Topic);
+            if (books == null || books.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(books);
+        }
+    }
+}
