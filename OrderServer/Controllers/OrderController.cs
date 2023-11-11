1f2891ac8da8524bc0241d492d23f7bb519ba7e7
Mohammad Omar <mohammad.m.omar.01@gmail.com>
2023-11-11 12:17:44 +0200

    Build OrderServer With DockerFile

---
+++ OrderServer/Controllers/OrderController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderServer.Models;
+using OrderServer.Utilites;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace OrderServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        OrderRepoistory OrderRepoistory { get; set; } = new OrderRepoistory();
+        string path
+        {
+            get { return Environment.GetEnvironmentVariable("MYURL"); }
+        }
+
+        [HttpPost("Book/{BookID}")]
+        public async Task<ActionResult> PurchuseBook([FromRoute] int BookID)
+        {
+            var getPath = path + $"/Book/{BookID}";
+            var postPath = getPath;
+            Console.WriteLine($"getPath is :{getPath}");
+            Console.WriteLine($"postPath is :{postPath}");
+
+            var response = Utility.FetchApiResponse(getPath);
+            Console.WriteLine("Result it " + response);
+            if (response == null)
+            {
+                return NotFound("Resource Not Found");
+            }
+            Book book = JsonSerializer.Deserialize<Book>(response);
+            Console.WriteLine($"BookId is :{book.bookId}");
+
+            if (Utility.CheckIfStockIsVaild(book))
+            {
+                HttpResponseMessage responseText = await Utility.SendPostApi(postPath, book);
+                Console.WriteLine("PostApiResponseis:" + responseText.StatusCode);
+                if (responseText.IsSuccessStatusCode)
+                {
+                    string responseContent = await responseText.Content.ReadAsStringAsync();
+                    PurchaseBook(book);
+
+                    return Created("Succefully Purchsed", book);
+                }
+                return BadRequest();
+            }
+
+            return Ok("Book is Not Available");
+        }
+
+        private void PurchaseBook(Book book)
+        {
+            var orders = OrderRepoistory.ReadCsvFile();
+            Order order = new Order
+            {
+                BookId = book.bookId,
+                date = DateOnly.FromDateTime(DateTime.Now)
+            };
+            if (orders == null)
+            {
+                order.OrderId = 1;
+            }
+            else
+            {
+                order.OrderId = orders.LastOrDefault().OrderId + 1;
+            }
+
+            orders.Add(order);
+            OrderRepoistory.WriteCsvFile(orders);
+        }
+    }
+}
---
+++ OrderServer/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderServer.Models
+{
+    public class Book
+    {
+        public int bookId { get; set; }
+        public string title { get; set; } = string.Empty;
+        public int stock { get; set; }
+        public int price { get; set; }
+        public string category { get; set; }
+    }
+}
